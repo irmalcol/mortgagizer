2ea97481d16689770500de9f00a1323f5d25fd07
Ian Malcolm <irmalcol@gmail.com>
2021-01-09 17:42:59 -0700

    Add HeaderItem component

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header-value">{{ this.value }}</div>
+  <div class="header-label">{{ this.label }}</div>
+</template>
+
+<script>
+export default {
+  name: "HeaderItem",
+  props: {
+    value: String,
+    label: String
+  }
+};
+</script>
