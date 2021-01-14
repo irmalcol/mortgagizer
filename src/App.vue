3e9d20262bf7dc383d996648d3b15d17a7bfed3f
Ian Malcolm <irmalcol@gmail.com>
2021-01-14 08:23:51 -0700

    Format and inject data into header row (COB still outstanding)

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
   <div class="header-row">
-    <HeaderItem value="$123,456" label="Outstanding Principal" />
+    <HeaderItem
+      :value="formattedStartingPrincipal"
+      label="Outstanding Principal"
+    />
   </div>
   <div class="header-row">
-    <HeaderItem value="9.99%" label="Interest Rate" />
-    <HeaderItem value="$8,888" label="Monthly Payment" />
-    <HeaderItem value="22 yrs, 7 mo" label="Time Remaining" />
+    <HeaderItem :value="formattedAnnualInterestRate" label="Interest Rate" />
+    <HeaderItem :value="formattedMonthlyPayment" label="Monthly Payment" />
+    <HeaderItem :value="formattedTimeRemaining" label="Time Remaining" />
     <HeaderItem value="$2,000" label="5 yr COB" />
     <HeaderItem value="$50,000" label="Total COB" />
   </div>
@@ -48,13 +51,48 @@ export default {
       principalPaymentLabel: "Principal",
       extraPaymentLabel: "Extra Payment",
       remainingPrincipalLabel: "Remaining Principal",
-      totalCOBLabel: "Cost of Borrowing"
+      totalCOBLabel: "Cost of Borrowing",
+      startingPrincipal: 500000,
+      annualInterestRate: 0.05,
+      monthlyPayment: 5000
     };
+  },
+  computed: {
+    formattedTimeRemaining() {
+      if (this.mortgage.length < 1) {
+        return "0 years, 0 months";
+      } else {
+        const lastPaymentData = this.mortgage[this.mortgage.length - 1];
+        const years = lastPaymentData.years;
+        const months = lastPaymentData.months % 12;
+        return `${years} years, ${months} months`;
+      }
+    },
+    formattedStartingPrincipal() {
+      return this.formatCurrency(this.startingPrincipal);
+    },
+    formattedAnnualInterestRate() {
+      return this.formatPercent(this.annualInterestRate);
+    },
+    formattedMonthlyPayment() {
+      return this.formatCurrency(this.monthlyPayment);
+    }
   },
   methods: {
     generateMortgageData() {
       const mortgage = new MortgageCalculator(500000, 0.05, 2);
       this.mortgage = mortgage.generateMortgageData(4000);
+    },
+    formatCurrency: function(amount) {
+      const formatter = new Intl.NumberFormat("en-CA", {
+        style: "currency",
+        currency: "CAD"
+      });
+      return formatter.format(amount);
+    },
+    formatPercent: function(amount) {
+      const convertedFromDecimalToPercent = amount * 100;
+      return `${convertedFromDecimalToPercent}%`;
     }
   }
 };
